71e91ac9e83e04e861e05ef867d48e50a045e7d4
Lucas Vinicius da Costa Santana <lvcs@ug3c43.windows.cin.ufpe.br>
2017-04-25 10:03:21 -0300

    AP1

---
+++ AP3/Q1.hs
@@ -0,0 +1,19 @@
+data Complex = Complex Double Double
+
+instance Num Complex where
+    (+) (Complex a b) (Complex c d) = Complex (a+c) (b+d)
+    (-) (Complex a b) (Complex c d) = Complex (a-b) (b-d)
+    negate (Complex a b) = Complex (-a) (-b)
+    (*) (Complex a b) (Complex c d) = Complex (a*c - b*d) (a*d + b*c)
+    abs (Complex a b) = Complex (sqrt (a*a + b*b)) 0
+    signum (Complex a b) = Complex (a / md) (b / md)
+        where md = sqrt(a*a + b*b)
+    fromInteger a = Complex (fromInteger (a)) 0
+
+instance Show Complex where
+    show (Complex a b) | b < 0 = show a ++ " - " ++ show (-b) ++ "i"
+                       | otherwise = show a ++ " + " ++ show b ++ "i"
+
+instance Eq Complex where
+    (==) (Complex a b) (Complex c d) = (a == c) && (b == d)
+
--- aula3.hs
+++ aula3.hs
@@ -2,6 +2,11 @@ type Ponto = (Float, Float, Float)
 type Nome  = String
 type Tamanho = Int
 data Diretorio = Pasta Nome [Diretorio] | Arquivo Nome Tamanho
+data Complex  = Complex Double Double
+
+instance Show Complex where
+    show (Complex real img) | img >= 0 = show real ++ " + " ++ show img ++ "i"
+                            | otherwise = show real ++ " - " ++ show (-img) ++ "i"
 
 distancia :: Ponto -> Ponto -> Float
 distancia (x1, y1, z1) (x2, y2, z2) = sqrt(x*x + y*y + z*z)
@@ -15,4 +20,5 @@ sumTuple (x, y) (k, j) = (x+k, y+j)
 size :: Diretorio -> (Tamanho, Int)
 size (Arquivo nome tamanho) = (tamanho, 1)
 size (Pasta nome diretorios) = foldl (sumTuple) ((0, 0)) (map (size) (diretorios))
+
 
